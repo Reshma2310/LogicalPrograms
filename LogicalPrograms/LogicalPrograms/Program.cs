daef169e23db8b95054d278cb862369cfd5cc4b2
Reshma2310 <dreshma253@gmail.com>
2022-05-20 13:31:39 +0500

    [Reshma] - Entered number is Perfect or not

--- LogicalPrograms/LogicalPrograms/Program.cs
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -1,4 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-LogicalPrograms.Fibonacci fibonacci = new LogicalPrograms.Fibonacci();
-fibonacci.FibonacciSeries();
+Console.WriteLine("1. Fibonacci\n2. Perfect Number");
+int input = Convert.ToInt32(Console.ReadLine());
+switch(input)
+{
+    case 1:
+        LogicalPrograms.Fibonacci fibonacci = new LogicalPrograms.Fibonacci();
+        fibonacci.FibonacciSeries();
+        break;
+    case 2:
+        LogicalPrograms.PerfectNumber perfect = new LogicalPrograms.PerfectNumber();
+        perfect.Perfect();
+        break;
+    default:
+        Console.WriteLine("Invalid Input");
+        break;
+}
