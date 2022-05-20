76726c6cb4fd32f55403bb6053937427582a1069
Reshma2310 <dreshma253@gmail.com>
2022-05-20 19:49:59 +0500

    [Reshma] - Stopwatch Simulation

--- LogicalPrograms/LogicalPrograms/Program.cs
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("1. Fibonacci\n2. Perfect Number\n3. Prime Numbers between Range\n4. Reverse a Number\n5. Coupon Number");
+Console.WriteLine("1. Fibonacci\n2. Perfect Number\n3. Prime Numbers between Range\n4. Reverse a Number\n5. Coupon Number\n6. Stopwatch Simulation");
 int input = Convert.ToInt32(Console.ReadLine());
 switch(input)
 {
@@ -20,8 +20,12 @@ switch(input)
         number.Reverse();
         break;
     case 5:
-        LogicalPrograms.CouponNumbers coupon=new LogicalPrograms.CouponNumbers();
+        LogicalPrograms.CouponNumbers coupon = new LogicalPrograms.CouponNumbers();
         coupon.Coupon();
+        break;
+    case 6:
+        LogicalPrograms.StopWatch watch = new LogicalPrograms.StopWatch();
+        watch.StopwatchOutput();
         break;
     default:
         Console.WriteLine("Invalid Input");
